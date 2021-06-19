c85ca71217a431408eb6f27c56b3bb6b5519157d
Remruat Thanga <remruatthanga.pro@gmail.com>
2021-06-19 20:51:00 +0630

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {BreadcrumbModule} from 'primeng/breadcrumb';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BreadcrumbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
